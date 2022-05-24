8993dc1f59f009c3ff860c9230e4bcb1202435bb
Luis <luisxciv@gmail.com>
2022-05-24 15:43:56 -0500

    initial commit
    now saves the folders as the product name

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
+
 # YupooDownloader
+## Setup
+
+Ensure you have python 3.6+ installed.
+
+```
+pip install -r requirements.txt
+
+```
+## enter your yupoo URL's into the JSON file, will enter the entire yupoo not individual albums. You can also set a delimiter for the number of albums to download.
+
+details.json
+
+## Running the script
+
+```
+python3 YupooPhotoDownloader.py
+
+```
---
+++ yupooDownloader.py
@@ -0,0 +1,147 @@
+# coding=utf-8
+
+from ctypes.wintypes import PINT
+from retrying import retry
+import pandas as pd
+import os
+import requests
+from bs4 import BeautifulSoup
+import csv
+import json
+
+with open('details.json')as f:
+    data = json.load(f)
+for state in data["yupoos"]:
+    break
+
+def getAlbumURLS():
+
+    f = open("albumURLs.csv", "w", newline="", encoding="utf-8")
+    #os.system("attrib +h albumURLs.csv")
+    writer = csv.writer(f, delimiter=' ', quoting=csv.QUOTE_MINIMAL)
+
+    url = state['yupoo_link']
+    text = url
+
+    head, sep, tail = text.partition('x.yupoo.com')
+    print("Downloading photos from site: " + head + "x.yupoo.com")
+
+    response = requests.get(url)
+    data = response.text
+    soup = BeautifulSoup(data, features="lxml") #soup = BeautifulSoup(data, 'lxml')
+    writer.writerow(["LINKS"])
+
+    row1 = []
+    count=0
+    for link in soup.findAll('a', class_='album__main'):
+        count=count+1
+        q = (link.get('href'))
+
+        row1.append(q)
+    print("Found " + str(count) + " albums...")
+
+
+    for c in range(len(row1)):
+        writer.writerow([row1[c]])
+
+
+    f.close()
+    print("File with album URLS located in: " + os.getcwd())
+    print("Downloading images...")
+
+getAlbumURLS()
+
+
+@retry(stop_max_attempt_number=5)
+def createHandler(X):
+    try:
+        with open((str(X) + 'handler.csv'), 'w', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            #WINDOWS DIRECTORY READING FROM ORIGINAL REPO
+            #df = pd.read_csv(os.getcwd() + "\\bf3_strona.csv", sep=' ')
+            #WORKING DIR REF IN UNIX SYSTEMS
+            df = pd.read_csv('./albumURLs.csv')
+            TEXT = (df['LINKS'][X])
+
+            url = state['yupoo_link']
+            text = url
+            head, sep, tail = text.partition('x.yupoo.com')
+            url = head + "x.yupoo.com" + TEXT
+
+            print(url)
+
+            response = requests.get(url, timeout=None)
+            data = response.content
+            soup = BeautifulSoup(data, 'lxml')
+            search = soup.select('.image__landscape')
+            title = soup.find_all("h2")[0].get_text()
+
+           # writer.writerow([X])
+            writer.writerow([title])
+
+            for x in search:
+                q = x['data-src']
+                writer.writerow(['https:' + q])
+            search = soup.select('.image__portrait')
+            for x in search:
+                q = x['data-src']
+                writer.writerow(['https:' + q])
+    except :
+        #print("passing")
+        pass
+
+
+def imageDownloader(x):
+    try:
+        def create_directory(directory):
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+
+        def download_save(url, folder):
+            try:
+                create_directory(folder)
+                c = requests.Session()
+                c.get('https://photo.yupoo.com/')
+                c.headers.update({'referer': 'https://photo.yupoo.com/'})
+                res = c.get(url, timeout=None)
+                with open(f'{folder}/{url.split("/")[-2]}.jpg', 'wb') as f:
+                    f.write(res.content)
+            except:
+                pass
+        #WINDOWS
+        #file = pd.read_csv(os.getcwd() + '\\' + str(x) + "TESTY.csv")
+        #UNIX
+        file = pd.read_csv('./' + str(x) + "handler.csv")
+        count = 0
+        try:
+            for col in file.columns:
+
+                for url in file[col].tolist():
+                    count += 1
+                    if str(url).startswith("http"):
+                        download_save(url, col)
+                        count
+
+                print("Downloaded " + str(count) + " images.")
+        except:
+            pass
+        try:
+            #path = (os.getcwd() + '\\' + col)
+            path = ('./' + col)
+
+            files = os.listdir(path)
+            for index, file in enumerate(files):
+                os.rename(os.path.join(path, file), os.path.join(path, ''.join([str(index), 'big.jpg'])))
+        except:
+            pass
+
+    except:
+        pass
+
+
+for x in range(int(state['productCount'])):
+    createHandler(x)
+    imageDownloader(x)
+    os.remove(str(x) + 'handler.csv')
+
+
